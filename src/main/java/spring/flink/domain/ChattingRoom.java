0e2a576f8dd0811e1e203c85c92216cf157dc9b4
hcg0127 <kej431003@gmail.com>
2025-05-06 08:22:21 +0900

    [FEAT] ChattingRoom Entity 및 관련 Enums 생성

---
+++ src/main/java/spring/flink/domain/ChattingRoom.java
@@ -0,0 +1,27 @@
+package spring.flink.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import spring.flink.domain.common.BaseEntity;
+import spring.flink.domain.enums.ChattingRoomStatus;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ChattingRoom extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomName;
+
+    @Enumerated(EnumType.STRING)
+    private ChattingRoomStatus chattingRoomStatus;
+}
