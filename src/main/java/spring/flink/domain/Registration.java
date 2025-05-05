d98309bff08d6bdafb5ef9740617151ab8b1e9b9
hcg0127 <kej431003@gmail.com>
2025-05-06 08:17:16 +0900

    [FEAT] Registration Entity 및 관련 Enums 생성

---
+++ src/main/java/spring/flink/domain/Registration.java
@@ -0,0 +1,25 @@
+package spring.flink.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import spring.flink.domain.common.BaseEntity;
+import spring.flink.domain.enums.RegistrationStatus;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Registration extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RegistrationStatus registrationStatus;
+}
