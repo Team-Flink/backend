2986d38b3892ef029417ad017fe3f2331fc04d4f
hcg0127 <kej431003@gmail.com>
2025-05-06 08:19:58 +0900

    [FEAT] Message Entity 생성

---
+++ src/main/java/spring/flink/domain/Message.java
@@ -0,0 +1,26 @@
+package spring.flink.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import spring.flink.domain.common.BaseEntity;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Message extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String detailMessage;
+}
