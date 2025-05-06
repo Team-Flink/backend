b3bea23c08fb8812af296c4348f969f971c5ed40
ssongmina <thdalsdk5498@naver.com>
2025-05-06 20:19:30 +0900

    [FEAT] Inquiry Entity 및 관련 Enums 생성

---
+++ src/main/java/spring/flink/domain/Inquiry.java
@@ -0,0 +1,35 @@
+package spring.flink.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import spring.flink.domain.common.BaseEntity;
+import spring.flink.domain.enums.InquiryStatus;
+import spring.flink.domain.enums.InquiryType;
+
+@Entity
+@Getter
+@Builder
+@DynamicUpdate
+@DynamicInsert
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Inquiry extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    private InquiryType type;
+
+    @Enumerated(EnumType.STRING)
+    private InquiryStatus status;
+
+
+}
---
+++ src/main/java/spring/flink/domain/enums/InquiryType.java
@@ -0,0 +1,5 @@
+package spring.flink.domain.enums;
+
+public enum InquiryType {
+    UIUX, API, ADDITION
+}
