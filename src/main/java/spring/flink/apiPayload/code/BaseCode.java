40cdbc76206653a18c82ff7031fff2f24cfdc33b
hcg0127 <kej431003@gmail.com>
2025-05-06 09:19:32 +0900

    [FEAT] API 응답 통일 및 에러 핸들러 코드 추가

---
+++ src/main/java/spring/flink/apiPayload/code/BaseCode.java
@@ -0,0 +1,8 @@
+package spring.flink.apiPayload.code;
+
+public interface BaseCode {
+
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+}
---
+++ src/main/java/spring/flink/apiPayload/code/BaseErrorCode.java
@@ -0,0 +1,8 @@
+package spring.flink.apiPayload.code;
+
+public interface BaseErrorCode {
+
+    public ErrorReasonDTO getReason();
+
+    public ErrorReasonDTO getReasonHttpStatus();
+}
