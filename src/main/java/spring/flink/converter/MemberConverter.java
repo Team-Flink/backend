a9baa8a4a153dfe897fdd852904e0cdfdabdef85
ssongmina <thdalsdk5498@naver.com>
2025-05-26 17:20:46 +0900

    [FEAT] 회원가입/로그인 관련 컨버터 생성

---
+++ src/main/java/spring/flink/converter/MemberConverter.java
@@ -0,0 +1,33 @@
+package spring.flink.converter;
+
+import spring.flink.domain.Member;
+import spring.flink.web.dto.MemberRequestDTO;
+import spring.flink.web.dto.MemberResponseDTO;
+
+import java.time.LocalDateTime;
+
+public class MemberConverter {
+
+    public static Member toMember(MemberRequestDTO.MemberJoinDTO request){
+        return Member.builder()
+                .email(request.getEmail())
+                .password(request.getPassword())
+                .nickname(request.getNickname())
+                .phoneNumber(request.getPhoneNumber())
+                .build();
+    }
+
+    public static MemberResponseDTO.MemberJoinResponseDTO toJoinResultDTO(Member member){
+        return MemberResponseDTO.MemberJoinResponseDTO.builder()
+                .memberId(member.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+
+    public static MemberResponseDTO.MemberLoginResultDTO toTokenDTO(String token){
+        return MemberResponseDTO.MemberLoginResultDTO.builder()
+                .accessToken(token)
+                .build();
+    }
+
+}
