9705ff6bf9d301463219e9b376e1c0b610782af3
ssongmina <thdalsdk5498@naver.com>
2025-05-30 12:19:56 +0900

    [FEAT] ResponseEntity로 반환하도록 수정 및 로그아웃 메서드 추가

--- src/main/java/spring/flink/web/controller/MemberController.java
+++ src/main/java/spring/flink/web/controller/MemberController.java
@@ -2,10 +2,17 @@ package spring.flink.web.controller;
 
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import spring.flink.apiPayload.ApiResponse;
+import spring.flink.apiPayload.code.status.ErrorStatus;
+import spring.flink.apiPayload.exception.GeneralException;
 import spring.flink.converter.MemberConverter;
 import spring.flink.domain.Member;
 import spring.flink.service.MemberService;
@@ -15,6 +22,7 @@ import spring.flink.web.dto.MemberResponseDTO;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/member")
@@ -22,44 +30,57 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MemberController {
 
     private final MemberService memberService;
-    private final Map<String, Integer> emailCodeMap = new ConcurrentHashMap<>();
-
 
     @PostMapping("/signup")
     @Operation(summary = "회원가입 API")
-    public ApiResponse<MemberResponseDTO.MemberJoinResponseDTO> join(@Valid @RequestBody MemberRequestDTO.MemberJoinDTO request) throws Exception{
-        Member member = memberService.joinMember(request);
-        return ApiResponse.onSuccess(MemberConverter.toJoinResultDTO(member));
+    public ResponseEntity<ApiResponse<MemberResponseDTO.MemberJoinResponseDTO>> join(@Valid @RequestBody MemberRequestDTO.MemberJoinDTO request) throws Exception{
+        MemberResponseDTO.MemberJoinResponseDTO result = memberService.joinMember(request);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(result));
     }
 
     @PostMapping("/signup/email")
     @Operation(summary = "이메일 인증번호 전송 API")
-    public ApiResponse<?> sendEmail(@RequestParam String email) throws Exception{
-        int code = memberService.sendMessage(email);
-        emailCodeMap.put(email, code);
-        return ApiResponse.onSuccess(null);
+    public ResponseEntity<ApiResponse<?>> sendEmail(@RequestParam String email) throws Exception{
+        // 인증 번호 생성
+        memberService.sendMessage(email);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(null));
     }
 
     @GetMapping("/signup/email/verify")
     @Operation(summary = "이메일 인증번호 확인 API")
-    public ApiResponse<?> verifyEmail(@RequestParam String email, @RequestParam int userNum) throws Exception{
-        // 해시 맵 기반으로 구현 -> 스프링 자체 캐시 / redis?
-        int correctCode = emailCodeMap.get(email);
-        boolean isRight = true;
-        if(correctCode != userNum){
-            isRight = false;
-        }
-        // false인 경우 아니라고 해야함
-        System.out.println("userNum = " + userNum);
-        return ApiResponse.onSuccess(isRight);
+    public ResponseEntity<ApiResponse<?>> verifyEmail(@RequestParam String email, @RequestParam String userNum) throws Exception{
+        // 이메일 인증번호 확인
+        memberService.verifyEmailCode(email, userNum);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(null));
     }
 
     @PostMapping("/login")
     @Operation(summary = "로그인 API")
-    public ApiResponse<MemberResponseDTO.MemberLoginResultDTO> login(@RequestBody MemberRequestDTO.MemberLoginDTO request) throws Exception{
+    public ResponseEntity<ApiResponse<?>> login(@RequestBody MemberRequestDTO.MemberLoginDTO request) throws Exception{
         // 로그인 완료 시, 클라이언트에세 액세스 토큰과 리프레시 토큰 제공
-        String token = memberService.login(request); // 왜 login이 static이어야하지?
-        return ApiResponse.onSuccess(MemberConverter.toTokenDTO(token));
+        memberService.login(request);
+        String accessToken = memberService.getAccessToken();
+        String refreshToken = memberService.getRefreshToken();
+        // 헤더에 액세스, 리프레시 토큰
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", accessToken);
+        headers.add("Refresh-Token", refreshToken);
+        return ResponseEntity.ok().headers(headers).body(ApiResponse.onSuccess(null));
     }
+
+    @PostMapping("/logout")
+    @Operation(summary = "로그아웃  API")
+    public ResponseEntity<ApiResponse<?>> logout(HttpServletRequest request) throws Exception{
+        memberService.logoutMember(request);
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(null));
+    }
+
+    @PostMapping("/delete")
+    @Operation(summary = "회원 탈퇴 API")
+    public ResponseEntity<ApiResponse<?>> delete(@RequestHeader("Authorization") String accessToken){
+
+        return ResponseEntity.ok().body(ApiResponse.onSuccess(null));
+    }
+
 
 }
