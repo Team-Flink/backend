487dd9fc3fa72e59f00bcbaa9eed5bdd0300ebfc
ssongmina <thdalsdk5498@naver.com>
2025-06-04 17:05:13 +0900

    [FEAT] jwtProperties 파일과 합치고, validateToken() 메서드에서 토큰 상태별 예외 던지기

--- src/main/java/spring/flink/security/jwt/JwtTokenProvider.java
+++ src/main/java/spring/flink/security/jwt/JwtTokenProvider.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
@@ -20,22 +22,38 @@ import java.util.Date;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class JwtTokenProvider{
+public class JwtTokenProvider implements InitializingBean {
 
-    private final JwtProperties jwtProperties;
     private final MemberDetailService memberDetailService;
+
+    @Value("${spring.jwt.secret}")
+    private String secret;
+
+    private SecretKey secretKey;
+
+    @Value("${spring.jwt.token.access_expiration}")
+    private Long accessExpireMS;
+
+    @Value("${spring.jwt.token.refresh_expiration}")
+    private Long refreshExpireMS;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
 
     //  토큰을 생성
     public String makeToken(Long memberId, String email, int type){
         Date now = new Date();
-        Long ex = (type == 1) ? jwtProperties.getAccessExpireMS() :  jwtProperties.getRefreshExpireMS();
+        Long ex = (type == 1) ? accessExpireMS : refreshExpireMS;
+        String tokenType = (type == 1) ? "accessToken" : "refreshToken";
         return Jwts.builder()
                 .claim("memberId", memberId)
-                .claim("tokenType", "accessToken")
+                .claim("tokenType", tokenType)
                 .claim("email", email)
                 .setIssuedAt(now)
                 .setExpiration(new Date(now.getTime() + ex))
-                .signWith(Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes()), SignatureAlgorithm.HS256)
+                .signWith(secretKey)
                 .compact();
     }
 
@@ -50,18 +68,25 @@ public class JwtTokenProvider{
 
     public boolean validateToken(String token){
         try{
-            SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
-
             Jwts.parser()
                     .verifyWith(secretKey)
                     .build()
                     .parseSignedClaims(token);
             return true;
         }
-        catch(Exception e){
-            return false;
+        catch (SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+            throw new GeneralException(ErrorStatus.WRONG_TYPE_SIGNATURE);
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+            throw new GeneralException(ErrorStatus.TOKEN_EXPIRED);
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+            throw new GeneralException(ErrorStatus.WRONG_TYPE_TOKEN);
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+            throw new GeneralException(ErrorStatus.NOT_VALID_TOKEN);
         }
-
     }
 
     public Authentication getAuthentication(String token){
@@ -70,8 +95,6 @@ public class JwtTokenProvider{
     }
 
     public String getEmail(String token){
-        SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
-
         return Jwts.parser()
                 .verifyWith(secretKey)
                 .build()
@@ -81,7 +104,6 @@ public class JwtTokenProvider{
     }
 
     public boolean isExpired(String token) {
-        SecretKey secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
         return Jwts.parser()
                 .verifyWith(secretKey)
                 .build()
@@ -89,6 +111,16 @@ public class JwtTokenProvider{
                 .getPayload()
                 .getExpiration()
                 .before(new Date());
+    }
+
+    public long getExpiration(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getExpiration()
+                .getTime();
     }
 
 }
